a9813864834e56b89197b0d7f48d7c4b163dbd39
ayush20892 <ayush20892@gmail.com>
2021-01-15 17:06:49 +0530

    Seconf Impt Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var btndata = document.querySelector("#btn-data");
+var txtinput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+
+var serverURL ="api.openweathermap.org/data/2.5/weather?appid=10ec6923de1a815bc337ad1124aa4994&q="
+
+function getURL(text)
+{
+  return serverURL + text
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time")
+}
+
+
+function clickEvetHandler()
+{
+  var inputText = txtinput.value; // taking input
+
+  // calling server for processing
+  fetch(getURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+          var translatedText = json.main.temp;
+          outputDiv.innerText = translatedText; // output
+         })
+      .catch(errorHandler)
+};
+
+btndata.addEventListener("click", clickEvetHandler);
